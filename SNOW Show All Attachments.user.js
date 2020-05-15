cb5232d59d26c3100a45e6170edb4adb8f5389b4
techslogi <32103117+techslogi@users.noreply.github.com>
2020-05-15 10:17:34 -0300

    hides more attachments button

--- SNOW Show All Attachments.user.js
+++ SNOW Show All Attachments.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SNOW Show All Attachments
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.5
 // @updateURL    https://github.com/techslogi/SNOW-Show-All-Attachments/raw/master/SNOW%20Show%20All%20Attachments.user.js
 // @downloadURL	 https://github.com/techslogi/SNOW-Show-All-Attachments/raw/master/SNOW%20Show%20All%20Attachments.user.js
 // @description  Increases the size of the attachment list from SNOW.
@@ -12,15 +12,15 @@
 (function() {
     'use strict';
 
-	//Creates a function that will receive the css parameter as a string, creates the element with your chosen style and appends it to the current id, class, whatever you tell it to.
-	function addGlobalStyle(css) {
-		var head, style;
-		head = document.getElementsByTagName('head')[0];
-		if (!head) { return; }
-		style = document.createElement('style');
-		style.type = 'text/css';
-		style.innerHTML = css;
-		head.appendChild(style);
+    //Creates a function that will receive the css parameter as a string, creates the element with your chosen style and appends it to the current id, class, whatever you tell it to.
+    function addGlobalStyle(css) {
+        var head, style;
+        head = document.getElementsByTagName('head')[0];
+        if (!head) { return; }
+        style = document.createElement('style');
+        style.type = 'text/css';
+        style.innerHTML = css;
+        head.appendChild(style);
     }
 
     //Necessary after Orlando version
@@ -30,11 +30,16 @@
         attachment.classList.remove("hidden");
     }
 
-	//Header ids responsible for emails and their respective styles which will be overwritten.
-	addGlobalStyle('#header_attachment { overflow: hidden !important; height: auto !important; }');
-	addGlobalStyle('#header_attachment_list { max-height: 100px !important; width: 100% !important; overflow-y: scroll !important; padding-right: 17px !important; box-sizing: content-box !important;}');
-	//Styles for the list. Floats left so they're equal, same to their width and height. Also puts a grid around them.
-	addGlobalStyle('.attachment_list_items { display: inline-block !important; height: 30px !important; overflow: hidden !important; float: left !important; width: 300px !important; border: 1px solid !important; border-color: #bdc0c4 !important; }');
+    var badges = document.getElementsByClassName('plus_more');
+    if (badges[0]){
+        badges[0].classList.add("hidden");
+    }
+
+    //Header ids responsible for emails and their respective styles which will be overwritten.
+    addGlobalStyle('#header_attachment { overflow: hidden !important; height: auto !important; }');
+    addGlobalStyle('#header_attachment_list { max-height: 100px !important; width: 100% !important; overflow-y: scroll !important; padding-right: 17px !important; box-sizing: content-box !important;}');
+    //Styles for the list. Floats left so they're equal, same to their width and height. Also puts a grid around them.
+    addGlobalStyle('.attachment_list_items { display: inline-block !important; height: 30px !important; overflow: hidden !important; float: left !important; width: 300px !important; border: 1px solid !important; border-color: #bdc0c4 !important; }');
 	//No need to have this since all attachments are being shown.
 	addGlobalStyle('#more_attachments { display: none !important;}');
 	//This one shows after attaching something, and displaces the list. No need for it since we have the paperclip icon.
